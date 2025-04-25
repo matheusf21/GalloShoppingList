4af35f3cadd1461d500337cc89e88a2a233f7eb5
matheusf21 <matheusferreiradelima21@gmail.com>
2025-04-25 12:24:53 -0300

    second commit

--- GalloShoppingList/app/index.js
+++ GalloShoppingList/app/index.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImage}>
+        <Image 
+          source={require('../assets/eu.jpeg')}
+          style={styles.avatar}
+        />
+      </View>
+      <View style={styles.content}> 
+        <Text style={styles.title}>
+          Gallo Shopping List
+          </Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras e não esqueça mais nada ao sair de casa!
+          </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+
+      </View>
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -13,8 +31,49 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+  },
+  containerImage: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  avatar: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    reziseMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center', 
+  },
+  text: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  button: {
+    position: 'absolute',
+    bottom: '15%',
+    backgroundColor: '#000',
+    alignSelf: 'center',
+    borderRadius: 50,
+    paddingVertical: 15,
+    width: '60%',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
