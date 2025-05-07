a523ea95af45fe65605282c0d4a3f7840914b098
matheusf21 <matheusferreiradelima21@gmail.com>
2025-05-07 13:56:50 -0300

    thay que fez

---
+++ GalloShoppingList/app/_layout.jsx
@@ -0,0 +1,15 @@
+import {Stack} from "expo-router";
+import { StatusBar } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" />  options={{ headerShow: false }} />
+        <Stack.Screen name="home" /> options={{ headerShow: false }} />
+       
+      </Stack>
+      <StatusBar style="light"/>
+    </>
+  )
+}
--- GalloShoppingList/app/home.jsx
+++ GalloShoppingList/app/home.jsx
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {Ionicons} from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex:1 }}>
+      <ImageBackground
+        source={require('../assets/background.jpeg')}
+        style={{flex:1, JustifyContent: 'flex-start'}}
+        resizeMode='repeat'
+      >
+        <View styles={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <Ionicons name= "trash" size{32} color="fff" />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+    
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    padding: 25,
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alighItems:'center',
+    backgroundColor: '#000000c0',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius:30,
+
+  },
+  title: {
+    fontSize:26,
+    fontWeight:'bold',
+    color: '#fff'
+
+  }
+})
