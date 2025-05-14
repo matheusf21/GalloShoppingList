058962e93df1f446ab0da7b0adb4b8a043fa148b
matheusf21 <matheusferreiradelima21@gmail.com>
2025-05-14 14:01:44 -0300

    7º commit.

--- GalloShoppingList/app/home.jsx
+++ GalloShoppingList/app/home.jsx
@@ -1,87 +1,119 @@
-import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import {Ionicons} from '@expo/vector-icons';
+import { Alert, FlatList, ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
-  return (
-    <SafeAreaView style={{ flex:1 }}>
-      <ImageBackground
-        source={require('../assets/background.jpeg')}
-        style={{flex:1, JustifyContent: 'flex-start'}}
-        resizeMode='repeat'
-      >
-        <View styles={styles.header}>
-          <Text style={styles.title}>Lista de Produtos</Text>
-          <Ionicons name= "trash" size={32} color="fff" />
-        </View>
+  const [textInput, setTextInput] =   useState('');
+  const [items, setItems] = useState([]);
 
-        <View style={{ flex:1}}> </View>
+  const addItem = () => {
+    if (textInput == ''){
+      Alert.alert(
+        'Ocorreu um problema :(',
+        'Por favor, informe o nome do produto!'
+      );
+    } else {
+      const newItem = {
+        id: Date.now().toString(),
+        name: textInput,
+        bought: false
+      }
+      setItems([...items, newItem]);
+      setTextInput('');
+      console.log(items)
+    }
+  }
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ImageBackground
+      source={require('../assets/background.jpg')}
+      style={{flex: 1, justifyContent: 'flex-start'}}
+      resizeMode='repeat'
+      >
+        <View style={styles.header}>
+            <Text style={styles.title}>Lista de Produtos</Text>
+            <Ionicons name="trash" size={32} color="#fff" />
+        </View> 
+
+        <FlatList 
+          contentContainerStyle={{padding: 20, paddingBottom: 100, color:'#fff'}}
+          data={items}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => 
+            <Text style={{color:"#fff"}}>{item.name}</Text>
+          }
+        />
 
         <View style={styles.footer}>
           <View style={styles.inputContainer}>
-            <TextInput color="#fff" fontSize={18}></TextInput>
+            <TextInput
+              color="#fff"
+              fontSize={18} 
+              placeholder='Digite o nome do produto...'  
+              placeholderTextColor='#aeaeae'
+              value={textInput}
+              onChangeText={(text) => setTextInput(text)}
+            />
           </View>
-          <TouchableOpacity style={styles.iconContainer}>
-            <Ionicons name='add' size={36} color='#fff' ></Ionicons>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem} >
+            <Ionicons name='add' size={36} color='#fff' />
           </TouchableOpacity>
         </View>
 
       </ImageBackground>
     </SafeAreaView>
-    
   )
 }
 
 const styles = StyleSheet.create({
-  header: {
-    padding: 25,
-    paddingTop: 50,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alighItems:'center',
-    backgroundColor: '#000000c0',
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius:30,
+    header: {
+        padding: 25,
+        paddingTop: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#000000c0',
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
 
-  },
-  title: {
-    fontSize:26,
-    fontWeight:'bold',
-    color: '#fff'
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    backgroundColor:'#000000c0',
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
 
-  },
-  inputContainer: {
-    backgroundColor: "#000",
-    elevation: 40,
-    flex: 1,
-    height: 50,
-    marginVertical:20,
-    borderRadius: 30,
-    paddingHorizontal: 20,
-    justifyContent: 'center',
+    footer: {
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+      backgroundColor: '#000000c0',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+    },
 
+    inputContainer: {
+      backgroundColor: "#000",
+      elevation: 40,
+      flex: 1,
+      height: 50,
+      marginVertical: 20,
+      borderRadius: 30,
+      paddingHorizontal: 20,
+      justifyContent: 'center',
+    },
 
-
-  },
-  iconContainer: {
-    height: 50,
-    width: 50,
-    backgroundColor: '#000',
-    borderRadius: 25,
-    elevation: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-
-  }
+    iconContainer: {
+      height: 50,
+      width: 50,
+      backgroundColor: '#000',
+      borderRadius: 25,
+      elevation: 40,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
 })
